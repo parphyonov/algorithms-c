ca3463e2f193a12968b32ceca2e92967cdaff70c
Alexei Parphyonov <alexeiparphyonov@gmail.com>
2024-12-12 17:20:49 +0300

    added variable_swap.c

---
+++ algorithms/variable_swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 0, b = 0;
+
+  scanf("%d%d", &a, &b);
+
+  printf("Initial State: A{%d} B{%d}\n", a, b);
+
+  printf("(does the swap)\n");
+  int tmp = a;
+  a = b;
+  b = tmp;
+
+  printf("Final State: A{%d} B{%d}\n", a, b);
+
+  return 0;
+}
